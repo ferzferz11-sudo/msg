cd9c1f19ad5c06c67cdb0d04df8cac892bf8b95d
Pavel ferzz Davydov <ferzz@x-cart.com>
2026-05-27 10:46:12 +0400

    1.0.6.34 - version changed

--- server.go
+++ server.go
@@ -27,7 +27,7 @@ import (
 	"firebase.google.com/go/v4/messaging"
 )
 
-const ServerVersion = "1.0.6.33"
+const ServerVersion = "1.0.6.34"
 
 // server implements the gRPC ChatService interface
 type server struct {
