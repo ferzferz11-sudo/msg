1c5cc2cf3b74aa9df007c02f10b82defc1afaa98
Lavender Server <ferz@lavender.local>
2026-06-01 09:22:33 +0000

    chore: bump version to v1.1.0.5 + changelog

--- server.go
+++ server.go
@@ -27,7 +27,7 @@ import (
 	"firebase.google.com/go/v4/messaging"
 )
 
-const ServerVersion = "1.1.0.4"
+const ServerVersion = "1.1.0.5"
 
 // server implements the gRPC ChatService interface
 type server struct {
